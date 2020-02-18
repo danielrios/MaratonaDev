43653f2b71f6bc18d681ff1f0e4d4d6559f2715e
Daniel Rios <49802471+Dederios@users.noreply.github.com>
2020-02-18 16:10:35 -0300

    Feat:Index.html is only template

--- public/scripts.js
+++ public/scripts.js
@@ -3,3 +3,27 @@ document
     .addEventListener("click", function () {
         document.querySelector('.form').classList.toggle('hide');
     });
+
+var sectionElement = document.querySelector('#doadores');
+var inputElementName = document.querySelector('#nome');
+var inputElementBlood = document.querySelector('#sangue');
+
+function add() {
+    var donorsDiv = document.createElement('div');
+    donorsDiv.setAttribute('class', 'donor');
+
+    var bloodDiv = document.createElement('div');
+    bloodDiv.setAttribute('class', 'blood');
+
+    var nameP = document.createElement('p');
+
+    var textName = document.createTextNode(inputElementName.value);
+    var textBlood = document.createTextNode(inputElementBlood.value);
+
+    bloodDiv.appendChild(textBlood);
+    nameP.appendChild(textName);
+
+    donorsDiv.appendChild(bloodDiv);
+    donorsDiv.appendChild(nameP);
+    sectionElement.appendChild(donorsDiv);
+}
